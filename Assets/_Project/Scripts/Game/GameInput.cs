43c8408a36dea8256d46540cbc43bbeb2eaf2175
Kanstantsin Asinouski <kanstantsin.asinouski@gmail.com>
2026-01-13 22:50:21 +0100

    Changed sprites. Add weapon animation and logic

---
+++ Assets/_Project/Scripts/Game/GameInput.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameInput : MonoBehaviour
+{
+    private PlayerInputActions playerInputActions;
+
+    public static GameInput Instance { get; private set; }
+
+    public event EventHandler OnPlayerAttack;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Enable();
+
+        playerInputActions.Combat.Attack.started += Player_AttackStarted;
+    }
+
+    private void Player_AttackStarted(InputAction.CallbackContext obj)
+    {
+        OnPlayerAttack?.Invoke(this, EventArgs.Empty);
+    }
+
+    public Vector2 GetMovementVector()
+    {
+        return playerInputActions.Player.Move.ReadValue<Vector2>();
+    }
+
+    public Vector3 GetMousePosition()
+    {
+        Vector3 mousePos = Mouse.current.position.ReadValue();
+        return mousePos;
+    }
+}
---
+++ Assets/_Project/Scripts/Player/Player.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player Instance { get; private set; }
+
+    [SerializeField] private float movingSpeed = 5f;
+
+    private Rigidbody2D rb;
+
+    private readonly float minMovingSpeed = 0.1f;
+    private bool isRunning = false;
+    private Vector2 inputVector;
+
+    public bool IsRunning()
+    {
+        return isRunning;
+
+    }
+
+    public Vector3 GetPlayerPosition()
+    {
+        Vector3 playerPosition = Camera.main.WorldToScreenPoint(transform.position);
+        return playerPosition;
+    }
+
+    private void Awake()
+    {
+        Instance = this;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        GameInput.Instance.OnPlayerAttack += GameInput_OnPlayerAttack;
+    }
+
+    private void GameInput_OnPlayerAttack(object sender, EventArgs e)
+    {
+        ActiveWeapon.Instance.GetActiveWeapon().Attack();
+    }
+
+    public void Update()
+    {
+        inputVector = GameInput.Instance.GetMovementVector();
+    }
+
+    private void FixedUpdate()
+    {
+        NewInputSystem();
+    }
+
+    // New input system, PlayerInputActions
+    private void NewInputSystem()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        rb.MovePosition(rb.position + inputVector * (movingSpeed * Time.fixedDeltaTime));
+
+        if (Mathf.Abs(inputVector.x) > minMovingSpeed || Mathf.Abs(inputVector.y) > minMovingSpeed)
+        {
+            isRunning = true;
+        }
+        else
+        {
+            isRunning = false;
+        }
+    }
+}
